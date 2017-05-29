9ada0923de9d1b2b4ae786307669eda077296120
pedromiguelcoelho <pedromiguelcoelho@ua.pt>
2017-05-29 17:26:27 +0100

    Changed dir

---
+++ src/main/java/com/project/finalproject/finalBean.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.project.finalproject;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import javax.inject.Named;
+import javax.enterprise.context.ApplicationScoped;
+import com.rabbitmq.client.*;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.CategoryAxis;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.LineChartModel;
+
+/**
+ *
+ * @author brito
+ */
+@Named(value = "finalBean")
+@ApplicationScoped
+public class finalBean {
+
+    private final static String QUEUE_NAME = "hello";
+    private ConnectionFactory factory;
+    private Connection connection;
+    private Channel channel;
+    private Consumer consumer;
+    private LineChartModel chart;
+    private ChartSeries cs = new ChartSeries();
+    private int count = 0;
+
+    /**
+     * Creates a new instance of finalBean
+     */
+    public finalBean(LineChartModel chart) throws IOException {
+        factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        this.chart = chart;
+        cs.setLabel("Heart Rate");
+        initConsumer();
+    }
+
+    private void initConsumer() throws IOException {
+        consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                String message = new String(body, "UTF-8");
+                setValue(message);
+                setCView(message);
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+    }
+    private String value;
+
+    public void setValue(String v) {
+        value = v;
+        if(Float.parseFloat(value)>150){
+            count++;
+        }
+    }
+
+    public String getValue() {
+        return value;
+    }
+    
+    public String getCount(){
+        return Integer.toString(count);
+    }
+
+    public void setCView(String value) {
+        SharedChart.updateChart(Math.round(Float.parseFloat(value)));
+    }
+    
+    public LineChartModel getCView(){
+        return SharedChart.getChart();
+    }
+}
